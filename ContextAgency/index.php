b5e73c9b529333c45a75e19582073e325a1795ac
ShaolinCzar <47002932+ShaolinCzar@users.noreply.github.com>
2021-03-17 17:04:38 +0300

    Context

---
+++ ContextAgency/index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.default.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" 
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" 
+    crossorigin="anonymous">
+    <script
+  src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+  crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="header">
+    <div class="logo"></div>
+    <div class="nav">
+    <a href="#why_us" id="1_id">О нас</a>
+    <a href="#how" id="2_id">Работы</a>
+    <a href="#cases" id="3_id">Кейсы</a>
+    <a href="#prices" id="4_id">Тарифы</a>
+    <a href="#promises" id="5_id">Гарантии</a>
+    <a href="#inf" id="6_id">Контакты</a>
+    </div>
+    <div class="number">+7 545 632 5412</div>
+    <button id="order_but" onclick="window.location='#feedback'">Заказать</button>
+    </div>
+    <div class="welcome">
+    <span>Some agency</span><br>
+    <span>Мы - гении во всём, что вам может пригодиться</span><br>
+    <span>Увеличиваем статистику продаж и всё, что захотите</span>
+    </div>
+    <div class="why_us" id="why_us">
+        <div class="wh">Почему следует выбрать нас?</div>
+        <div class="wl1"><span><i class="fas fa-ad fa-5x" aria-hidden="true"></i></span></div>
+        <div class="wl2"><span><i class="fas fa-ad fa-5x" aria-hidden="true"></i></span></div>
+        <div class="wl3"><span><i class="fas fa-ad fa-5x" aria-hidden="true"></i></span></div>
+        <div class="wd1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, quam! <p>Lorem ipsum dolor sit amet.</p></div>
+        <div class="wd2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, quam! <p>Lorem ipsum dolor sit amet.</p></div>
+        <div class="wd3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, quam! <p>Lorem ipsum dolor sit amet.</p></div>
+        <div class="wp"></div>
+    </div>
+    <div class="how" id="how">
+        <div class="hh">Как мы сможем это реализовать?</div>
+        <div class="hl1"><span><i class="fas fa-ad fa-5x" aria-hidden="true"></i></span></div>
+        <div class="hl2"><span><i class="fas fa-ad fa-5x" aria-hidden="true"></i></span></div>
+        <div class="hl3"><span><i class="fas fa-ad fa-5x" aria-hidden="true"></i></span></div>
+        <div class="hd1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, ipsa?<p>Lorem ipsum dolor sit amet consectetur.</p></div>
+        <div class="hd2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, ipsa?<p>Lorem ipsum dolor sit amet consectetur.</p></div>
+        <div class="hd3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, ipsa?<p>Lorem ipsum dolor sit amet consectetur.</p></div>
+    </div>
+    <div class="sert">
+        <div class="sert_text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Odio porro nobis iure impedit molestiae officiis, minus soluta nemo vel quibusdam, 
+            nostrum tempora voluptatibus velit delectus aliquid facilis sed facere a cum dolorem! 
+            Error, aliquam magni minus quas, itaque quae corrupti quod earum reiciendis vero nisi 
+            assumenda. Doloribus, veritatis sit ex maxime eius aspernatur odio nisi provident, officiis 
+            ullam illum? Eum!</div>
+        <div class="sert_img"></div>
+    </div>
+    <div class="ch" id="cases"><span>Наши кейсы</span><br><span>Избранные кейсы из каталога наших работ</span></div>
+    <div class="cases1_4">
+            <div class="c1">
+            <div class="chead">Название кейса</div>
+            <div class="cdes1">Первое описание<p>Характеристика</p></div>
+            <div class="cdes2">Второе описание<p>Характеристика</p></div>
+            <div class="cdes3">Третье описание<p>Характеристика</p></div>
+        </div>
+        <div class="c2"><div class="chead">Название кейса</div>
+            <div class="cdes1">Первое описание<p>Характеристика</p></div>
+            <div class="cdes2">Второе описание<p>Характеристика</p></div>
+            <div class="cdes3">Третье описание<p>Характеристика</p></div></div>
+        <div class="c3"><div class="chead">Название кейса</div>
+            <div class="cdes1">Первое описание<p>Характеристика</p></div>
+            <div class="cdes2">Второе описание<p>Характеристика</p></div>
+            <div class="cdes3">Третье описание<p>Характеристика</p></div></div>
+        <div class="c4"><div class="chead">Название кейса</div>
+            <div class="cdes1">Первое описание<p>Характеристика</p></div>
+            <div class="cdes2">Второе описание<p>Характеристика</p></div>
+            <div class="cdes3">Третье описание<p>Характеристика</p></div></div>
+    </div>
+    <div class="big_cases">
+        <div class="c5">
+            <div class="c5h">l</div>
+            <div class="c5t"><p>Первый заголовок</p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, possimus. Unde ea incidunt sed. Consequuntur facere voluptas blanditiis veritatis officia, mollitia esse, dolorum aperiam perferendis, voluptate doloremque iure deserunt voluptatum?
+        <p>Второй заголовок</p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis possimus blanditiis eum quisquam et aspernatur molestias autem, earum consequatur debitis dolore asperiores sunt reprehenderit, laborum suscipit doloribus aperiam expedita eaque.
+        <p>Третий заголовок</p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptates voluptate officia nemo cum, eligendi dolore temporibus vitae maxime non nesciunt.
+        </div>
+        </div>
+        <div class="c6">
+            <div class="c6h"></div>
+            <div class="c6t"><p>Первый заголовок</p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, possimus. Unde ea incidunt sed. Consequuntur facere voluptas blanditiis veritatis officia, mollitia esse, dolorum aperiam perferendis, voluptate doloremque iure deserunt voluptatum?
+        <p>Второй заголовок</p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis possimus blanditiis eum quisquam et aspernatur molestias autem, earum consequatur debitis dolore asperiores sunt reprehenderit, laborum suscipit doloribus aperiam expedita eaque.
+        <p>Третий заголовок</p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptates voluptate officia nemo cum, eligendi dolore temporibus vitae maxime non nesciunt.
+        </div>
+        </div>
+        <div class="c7">
+            <div class="c7h"></div>
+            <div class="c7t"><p>Первый заголовок</p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, possimus. Unde ea incidunt sed. Consequuntur facere voluptas blanditiis veritatis officia, mollitia esse, dolorum aperiam perferendis, voluptate doloremque iure deserunt voluptatum?
+        <p>Второй заголовок</p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis possimus blanditiis eum quisquam et aspernatur molestias autem, earum consequatur debitis dolore asperiores sunt reprehenderit, laborum suscipit doloribus aperiam expedita eaque.
+        <p>Третий заголовок</p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptates voluptate officia nemo cum, eligendi dolore temporibus vitae maxime non nesciunt.
+        </div>
+        </div>
+        <div class="c8">
+            <div class="c8h"></div>
+            <div class="c8t"><p>Первый заголовок</p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, possimus. Unde ea incidunt sed. Consequuntur facere voluptas blanditiis veritatis officia, mollitia esse, dolorum aperiam perferendis, voluptate doloremque iure deserunt voluptatum?
+        <p>Второй заголовок</p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis possimus blanditiis eum quisquam et aspernatur molestias autem, earum consequatur debitis dolore asperiores sunt reprehenderit, laborum suscipit doloribus aperiam expedita eaque.
+    </div>
+    <button id="start" onclick="window.location='#feedback'">Моя история начинается здесь!</button>
+    </div>
+</div>
+<div class="prices_header" id="prices">Тарифы на ведение вашего бизнеса</div>
+<div class="prices">
+    <div class="yandex">
+        <div class="yl"></div>
+        <p>Отличный способ прорекламироваться</p> <hr> <p>Отличный способ прорекламироваться</p> <hr> <p>Отличный способ прорекламироваться</p> <hr> <p>Любые пункты</p>
+    </div>
+    <div class="google">
+        <div class="gl"></div>
+        <p>Отличный способ прорекламироваться</p> <hr> <p>Отличный способ прорекламироваться</p> <hr> <p>Отличный способ прорекламироваться</p> <hr> <p>Любые пункты</p>
+    </div>
+</div>
+<div class="owl-carousel" id="promises">
+    <div class="slide1">Первое обещание <p>Описание обещания</p></div>
+    <div class="slide2">Второе обещание <p>описание обещания</p></div>
+</div>
+<div class="customers">Некоторые из наших заказчиков<br>
+<button id="add_name" onclick="window.location='#feedback'">Дайте людям узнать о себе!</button></div>
+<div class="owl-carousel cust_slider">
+    <div class="cs1">
+        <div class="cs11"><img src="img/first.jpeg" alt=""></div>
+        <div class="cs12"><img src="img/second.jpg" alt=""></div>
+        <div class="cs13"><img src="img/third.jpg" alt=""></div>
+        <div class="cs14"><img src="img/forth.png" alt=""></div>
+        <div class="cs15"><img src="img/5.jpeg" alt=""></div>
+        <div class="cs16"><img src="img/6.jpg" alt=""></div>
+    </div>
+    <div class="cs2">
+        <div class="cs21"><img src="img/7.jpg" alt=""></div>
+        <div class="cs22"><img src="img/8.jpg" alt=""></div>
+        <div class="cs23"><img src="img/9.jpeg" alt=""></div>
+        <div class="cs24"><img src="img/10.jpeg" alt=""></div>
+    </div>
+</div>
+<div class="feedback" id="feedback">
+    <div class="fbhead">Хотите получать больше заказов? <p>Обращайтесь к нам!</p></div>
+    <form id="form">
+        <input type="text" name="name" placeholder="Введите ваше имя" required>
+        <input type="email" name="e-mail" placeholder="Введите ваш E-mail" required>
+        <input type="text" name="phone" id="phone" placeholder="Введите ваш номер телефона">
+        <input type="submit" id="submit" value="Отправить">
+    </form>
+    <div class="rules_link">Все поля обязательны для заполнения, согласно <a href="rules.php">политике конфиденциальности</a></div>
+    <?
+            if(isset($_POST['name']) & isset($_POST['phone']) & isset($_POST['e-mail'])){
+                $to = "admitadmcc2017@gmail.com";
+                $from = "Context adv website";
+                $subject = "Контекстная реклама";
+                $subject = "=?utf-8?B?".base64_encode($subject)."?=";
+                $message = "Доброго времени суток! Пользователь с именем ".$_POST['name']." запросил рекламное продвижение.
+                Его номер телефона: ".$_POST['phone']." , его адрес электронной почты: ".$_POST['e-mail'].". Это письмо было
+                разрощено до таких объемов только с целью проверить, является ли важной частью наличие контента в письме. Если
+                это сработает, значит письмо должно быть информативным и, возможно, содержать определенный минимум по символам";
+                $headers  = "Content-type: text/html; charset=utf-8\r\n"; 
+                $headers .= "From: От кого письмо <from@example.com>\r\n"; 
+                $headers .= "Reply-To: reply-to@example.com\r\n"; 
+                mail($to, $subject, $message, $headers);
+            }
+            ?>
+</div>
+<div class="information" id="inf">
+    <div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2708.3518186839565!2d30.18861751769974!3d55.19300635228598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xeacbb280c8b2a941!2z0JHQuNC30L3QtdGBINGG0LXQvdGC0YAgItCa0LDQv9C40YLQsNC7Ig!5e0!3m2!1sru!2sby!4v1550329397001" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>    </div>
+    <div class="inf">
+        <p>Контакты</p><br>
+        <p>Агенство контекстной рекламы</p><br>
+        <p>some.email@some.some</p><br>
+        <p>Хотите обсудить всё лично?</p><br>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, nesciunt!</p><br>
+        <p>+3 324 342-3456</p>
+    </div>
+</div>
+</body>
+<script src="js/owl.carousel.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/jquery.maskedinput.min.js"></script>
+</html>
---
+++ ContextAgency/rules.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="rules">
+    <a href="index.php">Вернуться</a>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae quam eligendi magnam 
+        voluptatibus officiis aliquid iste aliquam sapiente cupiditate dolor, adipisci, reiciendis 
+        totam commodi laborum. Nam tempore voluptatum, cum quibusdam iste adipisci animi iusto eaque 
+        blanditiis quam reiciendis. Similique accusamus explicabo modi cupiditate minima! Consequuntur, 
+        dolor! Eveniet cumque tempora quae, rerum veniam recusandae qui reprehenderit consectetur nesciunt, 
+        laborum explicabo facere repellendus sit? Soluta architecto quo consequatur rerum ea illum! 
+        Blanditiis molestiae accusantium natus quasi reiciendis facilis fugiat corrupti laborum officiis.
+    </div>
+</body>
+</html>

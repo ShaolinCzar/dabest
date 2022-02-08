31cc72063a7b2b9de4e6fe2fba447e2b2751916b
ShaolinCzar <47002932+ShaolinCzar@users.noreply.github.com>
2022-02-08 17:03:37 +0300

    Delete localhost directory

--- localhost/first_form.php
+++
@@ -1,9 +0,0 @@
-<?
-                $message = "Пользователь с именем ".$_POST['name']." оставил заявку на консультацию. Его номер: ".$_POST['phone'].".";
-                $to = "shaolin.czar@gmail.com";
-                $from = "";
-                $subject = "Запрос на консультацию";
-                $subject = "=?utf-8?B?".base64_encode($subject)."?=";
-                $headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset=utf-8\r\n";
-                mail($to, $subject, $message);
-            ?>
